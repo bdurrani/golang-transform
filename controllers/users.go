d886cd350ecd194a536217f22272beb64e0cd8b6
Bilal Durrani <bilal.durrani@xero.com>
2022-04-24 21:50:42 -0400

    basic endpoint

---
+++ controllers/users.go
@@ -0,0 +1,25 @@
+package controllers
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type NameValueMap map[string]string
+type Controller struct {
+	Db NameValueMap
+}
+
+func (base *Controller) GetUser(context *gin.Context) {
+	context.String(http.StatusOK, "pong")
+	fmt.Printf("ClientIP: %s\n", context.ClientIP())
+
+	user := context.Params.ByName("name")
+	value, ok := base.Db[user]
+	if ok {
+		context.JSON(http.StatusOK, gin.H{"user": user, "value": value})
+	} else {
+		context.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"github.com/bdurrani/golang-transform/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+var db = map[string]string{
+	"test": "test-value",
+}
+
+func setupEngine() *gin.Engine {
+	gin.DisableConsoleColor()
+	router := gin.Default()
+
+	control := controllers.Controller{
+		Db: db,
+	}
+	router.GET("/user/:name", control.GetUser)
+	return router
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	router := setupEngine()
+
+	_ = router.SetTrustedProxies([]string{"127.0.0.1"})
+	// Listen and Server in 0.0.0.0:8080
+	_ = router.Run(":8080")
 }
